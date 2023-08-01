fe85eb1009ecd2c41c34a74fc97213c7219b00ab
andru666 <92786858+andru666@users.noreply.github.com>
2023-08-01 16:38:57 +0300

    01.52

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from kivy.uix.switch import Switch
 from kivy import base
 
 __all__ = 'install_android'
-__version__ = '0.01.51'
+__version__ = '0.01.52'
 
 mod_globals.os = platform
 if mod_globals.os == 'android':
